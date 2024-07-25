74f0fb3f06975bdfb0c37f7fbfe66668aa9d3409
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-24 23:04:39 -0300

    fix: zod

--- app/components/transaction-modal.vue
+++ app/components/transaction-modal.vue
@@ -1,33 +1,36 @@
 <template>
     <UModal v-model="isOpen">
         <UCard>
-            <template #header>
-                Add Transaction
-            </template>
-            <UFormGroup label="Transaction Type" :schema="schema" ref="form" @submit.prevent="save" :required="true"
-                name="Category" class="mb-4">
-                <USelect :options="transactionsType" placeholder="Select Transaction Type" v-model="state.type" />
-            </UFormGroup>
+            <UForm :state="state" :schema="schema" ref="form" @submit.prevent="save">
+                <template #header>
+                    Add Transaction
+                </template>
+                <UFormGroup label="Transaction Type" :required="true" name="Category" class="mb-4">
+                    <USelect :options="transactionsType" placeholder="Select Transaction Type" v-model="state.type" />
+                </UFormGroup>
 
-            <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
-                <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
-            </UFormGroup>
+                <UFormGroup label="Amount" :required="true" name="amount" class="mb-4">
+                    <UInput type="number" placeholder="Amount" v-model.number="state.amount" />
+                </UFormGroup>
 
-            <UFormGroup label="Transaction Date" :required="true" name="created_at" class="mb-4">
-                <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
-            </UFormGroup>
+                <UFormGroup label="Transaction Date" :required="true" name="created_at" class="mb-4">
+                    <UInput type="date" icon="i-heroicons-calendar-days-20-solid" v-model="state.created_at" />
+                </UFormGroup>
 
-            <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
-                <UInput placeholder="Description" v-model="state.description" />
-            </UFormGroup>
+                <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
+                    <UInput placeholder="Description" v-model="state.description" />
+                </UFormGroup>
 
-            <UFormGroup label="Category" :required="true" name="Category" class="mb-4" v-if="state.type === 'Expense'">
-                <USelect :options="categoriesOptions" placeholder="Description" v-model="state.category" />
-            </UFormGroup>
+                <UFormGroup label="Category" :required="true" name="Category" class="mb-4"
+                    v-if="state.type === 'Expense'">
+                    <USelect :options="categoriesOptions" placeholder="Description" v-model="state.category" />
+                </UFormGroup>
 
-            <div class="flex justify-end">
-                <UButton class="px-5" type="submit" color="black" variant="solid" label="Save" />
-            </div>
+                <div class="flex justify-end">
+                    <UButton class="px-5" type="submit" color="black" variant="solid" label="Save" />
+                </div>
+            </UForm>
+
         </UCard>
     </UModal>
 </template>
@@ -71,19 +74,33 @@ const schema = z.intersection(
 const form = ref()
 
 const save = async () => {
-    form.value.validate()
+    console.log("show me errors", form.value.errors.length)
+    if (form.value.errors.length) return
+    console.log(form)
 }
 
 const isOpen = computed({
     get: () => props.modelValue,
-    set: (value) => emit('update:modelValue', value)
+    set: (value) => {
+        if (!value) resetForm()
+        emit('update:modelValue', value)
+    }
 })
 
-const state = ref({
+const initialState = {
     type: undefined,
     amount: 0,
     created_at: undefined,
     description: undefined,
     category: undefined
+}
+
+const state = ref({
+    ...initialState
 })
+
+const resetForm = () => {
+    Object.assign(state.value, initialState)
+    form.value.clear()
+}
 </script>
