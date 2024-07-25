92dfb99e2cced5505d5b7d15748f48e110b72595
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-24 22:17:29 -0300

    feat: transaction modal

--- app/components/transaction-modal.vue
+++ app/components/transaction-modal.vue
@@ -4,25 +4,24 @@
         <template #header>
           Add Transaction
         </template>
-  
         <UFormGroup label="Transaction Type" :required="true" name="Category" class="mb-4">
-            <USelect type="transactionsType" :options="transactionsType" placeholder="Select Transaction Type"/>
+            <USelect :options="transactionsType" placeholder="Select Transaction Type"  v-model="state.type"/>
         </UFormGroup>
 
         <UFormGroup label="Amount" required="true" name="amount" class="mb-4">
-            <UInput type="number" placeholder="Amount"/>
+            <UInput type="number" placeholder="Amount"  v-model.number="state.amount"/>
         </UFormGroup>
 
         <UFormGroup label="Transaction Date" required="true" name="created_at" class="mb-4">
-            <UInput type="date" icon="i-heroicons-calendar-days-20-solid" />
+            <UInput type="date" icon="i-heroicons-calendar-days-20-solid"  v-model="state.created_at"/>
         </UFormGroup>
 
         <UFormGroup label="Description" hint="Optional" name="description" class="mb-4">
-            <UInput type="number" placeholder="Description"/>
+            <UInput  placeholder="Description"  v-model="state.description"/>
         </UFormGroup>
 
         <UFormGroup label="Category" :required="true" name="Category" class="mb-4">
-            <USelect type="categoryOptions" :options="categoriesOptions" placeholder="Description"/>
+            <USelect :options="categoriesOptions" placeholder="Description" v-model="state.category" />
         </UFormGroup>
 
         <div class="flex justify-end">
@@ -41,5 +40,13 @@ const emit = defineEmits(['update:modelValue'])
 const isOpen = computed({
     get: () => props.modelValue,
     set: (value) => emit('update:modelValue', value)
+})
+
+const state = ref({
+    type: undefined,
+    amount: 0,
+    created_at: undefined,
+    description: undefined,
+    category: undefined
 })
 </script>
