65cbf67abbf9eba6877c7850cab69f1fea3c4cbd
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-27 14:12:06 -0300

    refactor: seed with auth

--- seed.mjs
+++ seed.mjs
@@ -5,10 +5,14 @@ import { categoriesOptions } from './app/constants.js'
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
+    "INSERT SEED KEY",
     process.env.SUPABASE_KEY, {
     auth: { persistSession: false }
 }
 )
+
+const { data: { users }, error } = await supabase.auth.admin.listUsers()
+const userIds = users.map(user => user.id)
 
 const categories = ['Food', 'Housing', 'Car', 'Entertainment']
 
@@ -24,56 +28,58 @@ async function seedTransactions() {
     let transactions = []
 
     let transactionsQuantity = 10
+    for (const user of userIds) {
+        for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
+            for (let i = 0; i < transactionsQuantity; i++) {
+                const date = new Date(
+                    year,
+                    faker.number.int({ min: 0, max: 11 }),
+                    faker.number.int({ min: 1, max: 28 })
+                )
+                let type, category
+                const typeBias = Math.random();
 
-    for (let year = new Date().getFullYear(); year > new Date().getFullYear() - 2; year--) {
-        for (let i = 0; i < transactionsQuantity; i++) {
-            const date = new Date(
-                year,
-                faker.number.int({ min: 0, max: 11 }),
-                faker.number.int({ min: 1, max: 28 })
-            )
-            let type, category
-            const typeBias = Math.random();
+                if (typeBias < 0.85) {
+                    type = 'Expense'
+                    category = faker.helpers.arrayElement(categories)
+                } else if (typeBias < 0.95) {
+                    type = 'Income'
+                } else {
+                    type = faker.helpers.arrayElement(['Saving', 'Investment'])
+                }
 
-            if (typeBias < 0.85) {
-                type = 'Expense'
-                category = faker.helpers.arrayElement(categoriesOptions)
-            } else if (typeBias < 0.95) {
-                type = 'Income'
-            } else {
-                type = faker.helpers.arrayElement(['Saving', 'Investment'])
+                let amount
+                switch (type) {
+                    case 'Income':
+                        amount = faker.number.int({ min: 2000, max: 5000 })
+                        break;
+                    case 'Expense':
+                        amount = faker.number.int({ min: 2000, max: 5000 })
+                        break;
+                    case 'Saving':
+                    case 'Investment':
+                        amount = faker.number.int({ min: 5000, max: 10000 })
+                        break;
+                    default:
+                        amount = 0
+                }
+
+                transactions.push({
+                    created_at: date,
+                    amount,
+                    type,
+                    description: faker.lorem.sentence(),
+                    category: type === 'Expense' ? category : null,
+                    user_id: user
+                })
             }
 
-            let amount
-            switch (type) {
-                case 'Income':
-                    amount = faker.number.int({ min: 2000, max: 5000 })
-                    break;
-                case 'Expense':
-                    amount = faker.number.int({ min: 2000, max: 5000 })
-                    break;
-                case 'Saving':
-                case 'Investment':
-                    amount = faker.number.int({ min: 5000, max: 10000 })
-                    break;
-                default:
-                    amount = 0
-            }
-
-            transactions.push({
-                created_at: date,
-                amount,
-                type,
-                description: faker.lorem.sentence(),
-                category: type === 'Expense' ? category : null
-            })
+            const { error: insertError } = await supabase.from('transactions').upsert(transactions)
+            if (insertError)
+                console.error('Error inserting data into database', insertError)
+            else
+                console.error('Data inserted into database succesfully')
         }
-
-        const { error: insertError } = await supabase.from('transactions').upsert(transactions)
-        if (insertError)
-            console.error('Error inserting data into database', insertError)
-        else
-            console.error('Data inserted into database succesfully')
     }
 }
 
