7b8219842464d42e51a3753f1a9eb492ff96fdaa
alyson.ramos <ramos.alysonfarias@gmail.com>
2024-07-27 08:23:16 -0300

    feat: useSelectedTimePeriod

---
+++ app/composables/useSelectedTimePeriod.js
@@ -0,0 +1,43 @@
+import { startOfYear, endOfYear, sub, startOfDay, endOfDay, startOfMonth, endOfMonth } from 'date-fns'
+
+export const useSelectedTimePeriod = (period) => {
+    const dates = computed(
+        () => {
+            switch (period.value) {
+                case 'Yearly':
+                    return {
+                        current: {
+                            from: startOfYear(new Date()),
+                            to: new Date()
+                        },
+                        previous: {
+                            from: startOfYear(sub(new Date(), { years: 1 })),
+                            to: endOfYear(sub(new Date(), { years: 1 }))
+                        }
+                    }
+                case 'Monthly':
+                    return {
+                        current: {
+                            from: startOfMonth(new Date()),
+                            to: new Date()
+                        },
+                        previous: {
+                            from: startOfMonth(sub(new Date(), { months: 1 })),
+                            to: endOfMonth(sub(new Date(), { months: 1 }))
+                        }
+                    }
+                case 'Daily':
+                    return {
+                        current: {
+                            from: startOfDay(new Date()),
+                            to: new Date()
+                        },
+                        previous: {
+                            from: startOfMonth(sub(new Date(), { months: 1 })),
+                            to: startOfMonth(sub(new Date(), { months: 1 }))
+                        }
+                    }
+            }
+        }
+    )
+}
